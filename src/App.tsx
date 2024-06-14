d3115ed8de8320b5fe3a10cd6d57e8ce2f324c1d
paulzolik <paul.zolik@sky.com>
2024-06-14 14:13:04 +0100

    created header with a navbar with in it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Chicken from "./components/chicken";
-import './styles/App.css';
-// import Greeter from "./components/Greeter";
+import React from 'react';
 import Header from "./components/Header";
+import './styles/App.css';
 
-
-function App() {
-  return(
+const App: React.FC = () => {
+  return (
       <div className="App">
         <Header logoSrc="src/assets/images/image.png" logoAlt="logo" />        {/* login navbar */}
         {/* logo center with option navbar */}
@@ -17,7 +15,7 @@ function App() {
         {/* comment 3 boxes */}
         {/* section one of two with two photos */}
         {/* section one of two with two photos */}
-        {/* trendingh card 4 photos */}
+        {/* trendinh card 4 photos */}
         {/* two photo  */}
         {/* footer */}
    
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 // src/components/Header.tsx
 
 import React from 'react';
-// import './Header.css'; // Optional: Import a CSS file for styling
+// import '../styles/Header.css'; // Optional: Import a CSS file for styling
+import '../styles/Header.css';
+import Navbar from "../components/Navbar";
 
 interface HeaderProps {
   logoSrc: string;
@@ -14,13 +16,7 @@ const Header: React.FC<HeaderProps> = ({ logoSrc, logoAlt }) => {
       <div className="logo-container">
         <img src={logoSrc} alt={logoAlt} className="logo" />
       </div>
-      <nav className="secondary-navbar">
-        <ul>
-          <li><a href="/link1">Link 1</a></li>
-          <li><a href="/link2">Link 2</a></li>
-          <li><a href="/link3">Link 3</a></li>
-        </ul>
-      </nav>
+        <Navbar />
     </header>
   );
 };
