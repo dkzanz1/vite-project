c0ca819365628b0e7b5f20c3999876f9cb5638ca
paulzolik <paul.zolik@sky.com>
2024-09-22 14:38:51 +0100

    set up card

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,24 +40,26 @@ const Footer: React.FC = () => {
                 'Subheading 1 here',
 
             ]
-        },  
+        },
 
     ]
     return (
         <footer className="footer">
-            {sectionData.map((section, index)=>(
+            {sectionData.map((section, index) => (
                 <FooterSection key={index} title={section.title} items={section.items} />
             ))}
             <div className='footer-contact'>
                 <h3>Have a Question?</h3>
                 <p>
-                    Contact us at: <a href="mailto:classicanya@questions.co.uk">classicanya@questions.co.uk</a>
+                    Contact us at:<br></br>
+                    <a href="mailto:classicanya@questions.co.uk">classicanya@questions.co.uk</a>
                 </p>
             </div>
-            <h1>This is the footer please remove title once component complete</h1>
+
             {/*other component go below*/}
-            <p>© 2024 MyCompany. All rights reserved</p>
+            <p className='copyright'>© 2024 MyCompany. All rights reserved</p> 
         </footer>
+
     );
 };
 export default Footer;
